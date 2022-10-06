1eefca9f3c1edc69ba68945bd8e0da04f0b74b7a
ratuozzo <ratuozzo@gmail.com>
2022-10-06 21:18:35 +0200

    Clean up, added initial dependencies

---
+++ lib/presentation/matching_view/matching_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MatchingView extends StatelessWidget {
+  const MatchingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
